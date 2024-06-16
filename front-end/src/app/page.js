a0f06d79da70a12cdbf5e1f7f6faddc59d5da748
Victor <Shapovalenko.V.M@nmu.one>
2024-06-16 23:34:23 +0200

    Added TopBar

--- front-end/src/app/page.js
+++ front-end/src/app/page.js
@@ -3,6 +3,7 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import SignIn from "../../component/SignIn";
 import Blog from "../../component/LandingPage/Blog";
+import TopBar from "../../component/TopBar";
 
 export default function Home() {
   return (
