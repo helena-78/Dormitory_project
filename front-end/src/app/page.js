193151f9b40933eefee5c496cacd7b4fa038a9fb
Alexerererer <laow3334@gmail.com>
2024-06-16 17:00:22 +0300

    Landing page template addition

--- front-end/src/app/page.js
+++ front-end/src/app/page.js
@@ -2,9 +2,10 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import SignIn from "../../component/SignIn";
+import Blog from "../../component/LandingPage/Blog";
 
 export default function Home() {
   return (
-    <SignIn/>
+    <Blog/>
   );
 }
