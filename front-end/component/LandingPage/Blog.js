542f6045f4eb4accae5a747b0042269ed84aed03
Vlad5547 <73382987+Vlad5547@users.noreply.github.com>
2024-06-17 18:20:47 +0300

    slider logic

--- front-end/component/LandingPage/Blog.js
+++ front-end/component/LandingPage/Blog.js
@@ -1,11 +1,10 @@
-import * as React from 'react';
+import React from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
 import Grid from '@mui/material/Grid';
 import Container from '@mui/material/Container';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import XIcon from '@mui/icons-material/X';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Header from './Header_blog';
 import MainFeaturedPost from './MainFeaturedPost';
 import FeaturedPost from './FeaturedPost';
@@ -16,11 +15,21 @@ import post1 from './blog-post.1.md';
 import post2 from './blog-post.2.md';
 import post3 from './blog-post.3.md';
 
-
-
-const mainFeaturedPost = {
-  image: '/images/Dormitory.jpg',
-};
+const images = [
+  {
+    src: '/images/Dormitory.jpg',
+    alt: 'Dormitory',
+  },
+  {
+    src: '/images/AnotherImage.jpg',
+    alt: 'Another Image',
+  },
+  {
+    src: '/images/Dormitory.jpg',
+    alt: 'Dormitory',
+  },
+  // Add more images as needed
+];
 
 const featuredPosts = [
   {
@@ -66,21 +75,20 @@ const sidebar = {
   ],
 };
 
-// TODO remove, this demo shouldn't need to reset the theme.
 export default function Blog() {
   return (
-      <>
+    <>
       <CssBaseline />
       <Container maxWidth="lg">
-        <Header title="Blog"/>
+        <Header title="Blog" />
         <main>
-          <MainFeaturedPost post={mainFeaturedPost} />
-          <Grid container spacing={4}>
+          <MainFeaturedPost images={images} />
+          <Grid container spacing={4} sx={{ zIndex: 0, position: 'relative', marginTop: '20px' }}>
             {featuredPosts.map((post) => (
               <FeaturedPost key={post.title} post={post} />
             ))}
           </Grid>
-          <Grid container spacing={5} sx={{ mt: 3 }}>
+          <Grid container spacing={5} sx={{ mt: 3, zIndex: 0, position: 'relative' }}>
             <Main title="From the firehose" posts={posts} />
             <Sidebar
               title={sidebar.title}
--- front-end/component/LandingPage/MainFeaturedPost.js
+++ front-end/component/LandingPage/MainFeaturedPost.js
@@ -1,36 +1,85 @@
-import * as React from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Paper from '@mui/material/Paper';
-
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 
 function MainFeaturedPost(props) {
-  const { post } = props;
+  const { images } = props;
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
+    appendDots: dots => (
+      <div style={{ padding: '10px' }}>
+        <ul style={{ margin: 0 }}> {dots} </ul>
+      </div>
+    ),
+    customPaging: i => (
+      <div style={{ width: '30px', color: 'white', border: '1px white solid',}}>
+        {i + 1}
+      </div>
+    ),
+  };
 
   return (
-    <Paper
-      sx={{
-        position: 'relative',
-        backgroundColor: 'grey.800',
-        color: '#fff',
-        mb: 4,
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat',
-        backgroundPosition: 'center',
-        backgroundImage: `url(${post.image})`,
-        height: '400px',
-      }}
-    >
-      {/* Increase the priority of the hero background image */}
-      {<img style={{ display: 'none'}} src={post.image} alt={post.imageText}/>}
-        
-    </Paper>
+    
+      <Slider {...settings}>
+        {images.map((image, index) => (
+          <div key={index} style={{ height: '400px' }}>
+            <img 
+              src={image.src} 
+              alt={image.alt} 
+              style={{ 
+                width: '100%', 
+                height: '400px', 
+                objectFit: 'cover' 
+              }} 
+            />
+          </div>
+        ))}
+      </Slider>
+  
+  );
+}
+
+function NextArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, right: '10px', zIndex: 2,}}
+      onClick={onClick}
+    />
+  );
+}
+
+function PrevArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style,  left: '10px', zIndex: 2 }}
+      onClick={onClick}
+    />
   );
 }
 
 MainFeaturedPost.propTypes = {
-  post: PropTypes.shape({
-    image: PropTypes.string.isRequired,
-  }).isRequired,
+  images: PropTypes.arrayOf(
+    PropTypes.shape({
+      src: PropTypes.string.isRequired,
+      alt: PropTypes.string.isRequired,
+    })
+  ).isRequired,
 };
 
 export default MainFeaturedPost;
