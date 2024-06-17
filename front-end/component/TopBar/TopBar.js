0d707b62185c9cf52bb255192ecaa1c75b7286e3
Victor <Shapovalenko.V.M@nmu.one>
2024-06-17 14:34:25 +0200

    changed login icon

--- front-end/component/TopBar/TopBar.js
+++ front-end/component/TopBar/TopBar.js
@@ -2,10 +2,11 @@ import Image from "next/image";
 import * as React from "react";
 import Button from "@mui/material/Button";
 import styles from "./TopBar.css"
+import IconButton from "@mui/material/IconButton";
+import LoginIcon from '@mui/icons-material/Login';
 
 export default function () {
     const LogoImage = () => (<Image src="/Logo.png" height={70} width={70} alt=""/>);
-    const ProfileImage = () => (<Image src="/ProfileImage.png" height={30} width={30} alt=""/>);
 
     return (
             <div id={'topBar'}>
@@ -22,8 +23,8 @@ export default function () {
                     <div className="topBarButton">
                         <Button>Профіль</Button>
                     </div>
-                    <div id={'profileImage'}>
-                    <ProfileImage></ProfileImage>
+                    <div id={'loginImage'}>
+                    <LoginIcon sx={{color:'#1976d2'}}></LoginIcon>
                     </div>
                 </div>
             </div>
