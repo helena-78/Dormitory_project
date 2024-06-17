d1be501729911bbcf6a4c4c36350d503fd9d2bb1
Vlad5547 <73382987+Vlad5547@users.noreply.github.com>
2024-06-17 16:07:46 +0300

    Image main post

--- front-end/component/LandingPage/Blog.js
+++ front-end/component/LandingPage/Blog.js
@@ -19,12 +19,7 @@ import post3 from './blog-post.3.md';
 
 
 const mainFeaturedPost = {
-  title: 'Title of a longer featured blog post',
-  description:
-    "Multiple lines of text that form the lede, informing new readers quickly and efficiently about what's most interesting in this post's contents.",
-  image: 'https://source.unsplash.com/random?wallpapers',
-  imageText: 'main image description',
-  linkText: 'Continue reading…',
+  image: '/images/Dormitory.jpg',
 };
 
 const featuredPosts = [
@@ -34,7 +29,6 @@ const featuredPosts = [
     description:
       'This is a wider card with supporting text below as a natural lead-in to additional content.',
     image: 'https://source.unsplash.com/random?wallpapers',
-    imageLabel: 'Image Text',
   },
   {
     title: 'Post title',
