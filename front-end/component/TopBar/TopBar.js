7718445b26cf4f25f0761bc5cf4a26a009bc9313
Victor <Shapovalenko.V.M@nmu.one>
2024-06-17 20:42:05 +0200

    added footer

--- front-end/component/TopBar/TopBar.js
+++ front-end/component/TopBar/TopBar.js
@@ -6,27 +6,27 @@ import IconButton from "@mui/material/IconButton";
 import LoginIcon from '@mui/icons-material/Login';
 
 export default function () {
-    const LogoImage = () => (<Image src="/Logo.png" height={70} width={70} alt=""/>);
+    const LogoImage = () => (<Image src="/LogoTopBar.png" height={70} width={70} alt=""/>);
 
     return (
-            <div id={'topBar'}>
-                <div  className="topBarGroup">
-                    <LogoImage></LogoImage>
-                    <div className="topBarButton">
-                        <Button>Головна</Button>
-                    </div>
-                    <div className="topBarButton">
-                        <Button>Kонтакти</Button>
-                    </div>
+        <div id={'topBar'}>
+            <div className="topBarGroup">
+                <LogoImage></LogoImage>
+                <div className="topBarButton">
+                    <Button>Головна</Button>
                 </div>
-                <div className="topBarGroup">
-                    <div className="topBarButton">
-                        <Button>Профіль</Button>
-                    </div>
-                    <div id={'loginImage'}>
-                    <LoginIcon sx={{color:'#1976d2'}}></LoginIcon>
-                    </div>
+                <div className="topBarButton">
+                    <Button>Kонтакти</Button>
                 </div>
             </div>
+            <div className="topBarGroup">
+                <div className="topBarButton">
+                    <Button>Профіль</Button>
+                </div>
+                <div id={'loginImage'}>
+                    <LoginIcon sx={{color: '#1976d2'}}></LoginIcon>
+                </div>
+            </div>
+        </div>
     );
 }
--- front-end/src/app/page.js
+++ front-end/src/app/page.js
@@ -4,9 +4,10 @@ import styles from "./page.module.css";
 import SignIn from "../../component/SignIn";
 import Blog from "../../component/LandingPage/Blog";
 import TopBar from "../../component/TopBar/TopBar";
+import Footer from "../../component/Footer/Footer";
 
 export default function Home() {
-  return (
-    <TopBar/>
-  );
+    return (
+        <Footer/>
+    );
 }
