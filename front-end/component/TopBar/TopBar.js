f116e96461d76794f0ca1591a4dc77b66d9541d4
DanilHershelia <danil.gershelya4435@gmail.com>
2024-06-18 20:59:57 +0300

    ProfilePageNavigation

--- front-end/component/TopBar/TopBar.js
+++ front-end/component/TopBar/TopBar.js
@@ -1,30 +1,39 @@
+"use client";
+
 import Image from "next/image";
 import * as React from "react";
 import Button from "@mui/material/Button";
-import styles from "./TopBar.css"
+import styles from "./TopBar.css";
 import IconButton from "@mui/material/IconButton";
 import LoginIcon from '@mui/icons-material/Login';
+import { useRouter } from 'next/navigation';
 
-export default function () {
-    const LogoImage = () => (<Image src="/LogoTopBar.png" height={70} width={70} alt=""/>);
+export default function TopBar() {
+    const router = useRouter();
+
+    const handleProfileClick = () => {
+        router.push('/profile');
+    };
+
+    const LogoImage = () => (<Image src="/LogoTopBar.png" height={70} width={70} alt="Logo" />);
 
     return (
-        <div id={'topBar'}>
+        <div id="topBar">
             <div className="topBarGroup">
-                <LogoImage></LogoImage>
+                <LogoImage />
                 <div className="topBarButton">
                     <Button>Головна</Button>
                 </div>
                 <div className="topBarButton">
-                    <Button>Kонтакти</Button>
+                    <Button>Контакти</Button>
                 </div>
             </div>
             <div className="topBarGroup">
                 <div className="topBarButton">
-                    <Button>Профіль</Button>
+                    <Button onClick={handleProfileClick}>Профіль</Button>
                 </div>
-                <div id={'loginImage'}>
-                    <LoginIcon sx={{color: '#1976d2'}}></LoginIcon>
+                <div id="loginImage">
+                    <LoginIcon sx={{ color: '#1976d2' }} />
                 </div>
             </div>
         </div>
