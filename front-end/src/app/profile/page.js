c0fb8f325dce5f35340d49d55ff4fb9258ac3206
DanilHershelia <danil.gershelya4435@gmail.com>
2024-06-20 14:23:39 +0300

    Перадача даних з реєстраці та авторизації у мій профіль

--- front-end/src/app/profile/page.js
+++ front-end/src/app/profile/page.js
@@ -1,60 +1,70 @@
-// pages/profile.js
+"use client"
 import * as React from 'react';
-
-
-import styles from './Profile.module.css'
+import { useEffect, useState } from 'react';
+import styles from './Profile.module.css';
 
 const Profile = () => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const storedUser = JSON.parse(localStorage.getItem('user'));
+    if (storedUser) {
+      setUser(storedUser);
+    }
+  }, []);
+
+  if (!user) {
+    return <p>Завантаження...</p>;
+  }
+
   return (
-          
     <div className={styles.container}>
-    <div className={styles.profile}>
-      <div className={styles.avatar}>
-        <img className={styles.avatar} src="/profilephoto.png" alt="Avatar" />
-        <button className={styles.editProfileButton}>Редагувати профіль</button>
-      </div>
-      
-      <div className={styles.userData}>
-        <h2 className={styles.profileText}>Мій профіль</h2>
-        <div className={styles.form}>
-          <div className={styles.formRow}>
-            <div className={styles.formGroup}>
-              <label className={styles.profileLabel}>Ім'я</label>
-              <p className={styles.textBox}>Іван</p>
+      <div className={styles.profile}>
+        <div className={styles.avatar}>
+          <img className={styles.avatar} src="/profilephoto.png" alt="Avatar" />
+          <button className={styles.editProfileButton}>Редагувати профіль</button>
+        </div>
+
+        <div className={styles.userData}>
+          <h2 className={styles.profileText}>Мій профіль</h2>
+          <div className={styles.form}>
+            <div className={styles.formRow}>
+              <div className={styles.formGroup}>
+                <label className={styles.profileLabel}>Ім'я</label>
+                <p className={styles.textBox}>{user.firstName}</p>
+              </div>
+              <div className={styles.formGroup}>
+                <label className={styles.profileLabel}>Прізвище</label>
+                <p className={styles.textBox}>{user.lastName}</p>
+              </div>
+            </div>
+            <div className={styles.formRow}>
+              <div className={styles.formGroup}>
+                <label className={styles.profileLabel}>ID</label>
+                <p className={styles.textBox}>{user.id || '12345'}</p>
+              </div>
+              <div className={styles.formGroup}>
+                <label className={styles.profileLabel}>Телефон</label>
+                <p className={styles.textBox}>+38{user.phone}</p>
+              </div>
             </div>
             <div className={styles.formGroup}>
-              <label className={styles.profileLabel}>Прізвище</label>
-              <p className={styles.textBox}>Іванов</p>
+              <label className={styles.profileLabel}>Пошта</label>
+              <p className={styles.textBox}>{user.email}</p>
             </div>
-          </div>
-          <div className={styles.formRow}>
-            <div className={styles.formGroup}>
-              <label className={styles.profileLabel}>ID</label>
-              <p className={styles.textBox}>12345</p>
-            </div>
-            <div className={styles.formGroup}>
-              <label className={styles.profileLabel}>Телефон</label>
-              <p className={styles.textBox}>0999999999</p>
-            </div>
-          </div>
-          <div className={styles.formGroup}>
-            <label className={styles.profileLabel}>Пошта</label>
-            <p className={styles.textBox}>ivanov@gmail.com</p>
           </div>
         </div>
       </div>
-    </div>
-    <div className={styles.Myroom}>
-    <h2 className={styles.profileRoom}>Моя кімната</h2>
-    <div className={styles.room}>
-      <p>№511</p>
-    </div>
-    <div className={styles.roomImage}>
+      <div className={styles.Myroom}>
+        <h2 className={styles.profileRoom}>Моя кімната</h2>
+        <div className={styles.room}>
+          <p>№511</p>
+        </div>
+        <div className={styles.roomImage}>
           <img src="/8_bed_1st.jpg" alt="Room Image" />
         </div>
+      </div>
     </div>
-    
-  </div>
   );
 };
 
