3a6f3180101989f05bca8246f89d60bdef926ef9
Oleksandr Hus <sanagus04@gmail.com>
2024-06-25 09:48:59 +0300

    Aplications API

--- back-end/dormitoryBackend/dormitoryBackend/settings.py
+++ back-end/dormitoryBackend/dormitoryBackend/settings.py
@@ -31,12 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'status_check',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'dormitoryBackend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'UvAXlYgzkTzsnBhYqTdCOUuGATZuLiXu',
+        'HOST': 'roundhouse.proxy.rlwy.net',
+        'PORT': '51774',
     }
 }
 
--- back-end/dormitoryBackend/dormitoryBackend/urls.py
+++ back-end/dormitoryBackend/dormitoryBackend/urls.py
@@ -1,22 +1,12 @@
-"""
-URL configuration for dormitoryBackend project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
 from django.urls import path
+from status_check.views import create_application,  get_application, update_application_status, ApplicationsAPIView, delete_application
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('applications/', ApplicationsAPIView.as_view(), name='applications-list'),
+    path('applications/create/', create_application, name='create-application'),
+    path('applications/<int:pk>/', get_application, name='get-application'),
+    path('applications/<int:pk>/update/', update_application_status, name='update-application-status'),
+    path('applications/<int:pk>/', delete_application, name='delete_application'),
 ]
+
+
