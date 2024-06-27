7462e1a85f303ec8404b983671d4366c587155c8
Oleksandr Hus <sanagus04@gmail.com>
2024-06-28 00:37:11 +0300

    Bookings API

--- back-end/dormitoryBackend/dormitoryBackend/settings.py
+++ back-end/dormitoryBackend/dormitoryBackend/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -32,6 +36,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'status_check',
+    'bookings',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,13 +81,13 @@ WSGI_APPLICATION = 'dormitoryBackend.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'UvAXlYgzkTzsnBhYqTdCOUuGATZuLiXu',
-        'HOST': 'roundhouse.proxy.rlwy.net',
-        'PORT': '51774',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
--- back-end/dormitoryBackend/dormitoryBackend/urls.py
+++ back-end/dormitoryBackend/dormitoryBackend/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from status_check.views import create_application,  get_application, update_application_status, ApplicationsAPIView, delete_application
+from bookings import views
 
 urlpatterns = [
     path('applications/', ApplicationsAPIView.as_view(), name='applications-list'),
@@ -7,6 +8,11 @@ urlpatterns = [
     path('applications/<int:pk>/', get_application, name='get-application'),
     path('applications/<int:pk>/update/', update_application_status, name='update-application-status'),
     path('applications/<int:pk>/', delete_application, name='delete_application'),
+    path('bookings/', views.list_bookings, name='list_bookings'),
+    path('bookings/create/', views.create_booking, name='create_booking'),
+    path('bookings/<int:pk>/', views.get_booking, name='get_booking'),
+    path('bookings/<int:pk>/update/', views.update_booking, name='update_booking'),
+    path('bookings/<int:pk>/', views.delete_booking, name='delete_booking'),
 ]
 
 
