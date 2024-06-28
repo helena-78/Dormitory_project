6e3347603f24669fb83bf5d0fc40b6778e1a2fb4
Alexerererer <laow3334@gmail.com>
2024-06-28 09:26:01 +0300

    Admin payments page 1st run

--- front-end/component/AdminPanel/List/DynamicList.js
+++ front-end/component/AdminPanel/List/DynamicList.js
@@ -1,3 +1,4 @@
+'use client'
 import Typography from "@mui/material/Typography";
 import List from "@mui/material/List";
 import Divider from "@mui/material/Divider";
@@ -9,20 +10,21 @@ import Avatar from "@mui/material/Avatar";
 import ListItemText from "@mui/material/ListItemText";
 import Grid from "@mui/material/Grid";
 import * as React from "react";
-import {styled} from "@mui/material/styles";
+import { styled } from "@mui/material/styles";
 import styles from './DynamicList.css';
-import Link from "next/link";
+import { Box } from "@mui/material";
 
 export default function DynamicList(props) {
     let listItems = generateListItems(props);
+
     return (
-        <Grid item xs={12} md={6}>
-            <Typography sx={{mt: 4, mb: 2}} variant="h6" component="div" align="center">
+        <Grid item xs={'auto'} md={'auto'}>
+            <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div" align="center">
                 <b> Список {props.title}: </b>
             </Typography>
             <div>
                 <List>
-                    <Divider sx={{borderColor: '#333333'}}></Divider>
+                    <Divider sx={{ borderColor: '#333333' }}></Divider>
                     {listItems}
                 </List>
             </div>
@@ -33,39 +35,27 @@ export default function DynamicList(props) {
 function generateListItems(props) {
     let listItemsArray = [];
 
-    for (let i = 0; i < props.dataLength; i++) {
+    for (let i = 0; i < props.data.length; i++) {
         listItemsArray[i] =
-            <div primary={props.itemName + props.itemValues[i]} key={i} className="listElement">
+            <div key={i} className="listElement" onClick={() => props.onItemClick(props.data[i].student_id)}>
                 <ListItem
                     secondaryAction={
-                    <Link
-                        href={{
-                            pathname: props.editComponentUrl,
-                            query: "id=" + props.itemIDs[i]
-                        }}
-                    >
                         <IconButton edge="end">
-                            <BorderColorIcon sx={{color: '#1976d2'}}></BorderColorIcon>
+                            <BorderColorIcon sx={{ color: '#1976d2' }}></BorderColorIcon>
                         </IconButton>
-                    </Link>
                     }
                 >
                     <ListItemAvatar>
-                        <Avatar sx={{backgroundColor: '#1976d2'}}>
+                        <Avatar sx={{ backgroundColor: '#1976d2' }}>
                             {props.icon}
                         </Avatar>
                     </ListItemAvatar>
                     <ListItemText
-                        primary={props.itemName + props.itemValues[i]}
+                        primary={props.itemName + props.items[i]}
                     />
                 </ListItem>
             </div>
     }
 
-    listItemsArray= listItemsArray.sort(props.sortExpression);
-
     return listItemsArray;
 }
-
-
-
