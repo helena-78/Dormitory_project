7aa64c0000cf1618e98363879ecd561ce064a71d
Andrii <65673865+darkrp03@users.noreply.github.com>
2024-07-04 21:10:55 +0300

    Update models.py

--- back-end/dormitoryBackend/api/models.py
+++ back-end/dormitoryBackend/api/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.utils import timezone
-from status_check.models import Application
 
 class Student(models.Model):
     student_id = models.BigAutoField(primary_key=True)
@@ -10,7 +9,7 @@ class Student(models.Model):
     contact_number = models.CharField(max_length=10, null=True, blank=True)   
     gender = models.TextField(choices=[('Male', 'Male'), ('Female', 'Female')])
     room = models.ForeignKey('Room', null=True, blank=True, on_delete=models.SET_NULL)
-    application = models.ForeignKey(Application, null=True, blank=True, on_delete=models.SET_NULL, related_name='students')
+    application = models.ForeignKey('status_check.Application', null=True, blank=True, on_delete=models.SET_NULL, related_name='students')
     password = models.TextField()
 
 class Room(models.Model):
